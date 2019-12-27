a7549731feefc8fc338144228a2b8299515c6ba6
Yury Lapitsky <yury.lapitsky@gmail.com>
2019-12-27 21:27:34 +0100

    Implement basic data structures

--- Sources/ReactiveBeaverSwift/ReactiveBeaverSwift.swift
+++ Sources/ReactiveBeaverSwift/ReactiveBeaverSwift.swift
@@ -1,5 +1,3 @@
-import Combine
-
 struct Epub {
     let sha1: String
     let sourcePath: String
@@ -7,6 +5,50 @@ struct Epub {
     let spineElements: [AnyObject] // TODO: improve types here
     let manifestElements: [AnyObject] // TODO: improve types here
     let coverPath: String?
+    
+    class NavPointElement {
+        let level: String // FIXME type is not optimized here
+        var parentElement: NavPointElement?
+        let childElements: [NavPointElement]
+        
+        init(level: String, parentElement: NavPointElement? = nil, childElements: [NavPointElement] = []) {
+            self.level = level
+            self.parentElement = parentElement
+            self.childElements = childElements
+        }
+    }
+    
+    enum ManifestXMLKey: String {
+        case identifier = "id"
+        case href = "href"
+        case mediaType = "media-type"
+    }
+    
+    struct ManifestElement {
+        /// Should be unique inside manifest
+        let identifier: String
+
+        /// Relative URI string
+        let href: String
+
+        ///
+        let mediaType: String // FIXME: not optimal type
+    }
+    
+    struct Metadata {
+        let title: String
+        let creator: String
+        let identifier: String
+        let publisher: String
+        let contributor: String
+        let rights: String
+    }
+    
+    struct SpineElement {
+        let index: Int
+        let idRef: String
+        let fileName: String
+    }
 }
 
 final class ZipUnpacker {
