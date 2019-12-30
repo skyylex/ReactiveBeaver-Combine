647055a7987e609c44f9230aa245b98865a6ccd0
Yury Lapitsky <yury.lapitsky@gmail.com>
2019-12-30 22:56:14 +0100

    Setup moby-dick path in Parser unit tests

--- Tests/ReactiveBeaverSwiftTests/ParserTests.swift
+++ Tests/ReactiveBeaverSwiftTests/ParserTests.swift
@@ -2,6 +2,18 @@ import XCTest
 @testable import ReactiveBeaverSwift
 
 final class ParserTests: XCTestCase {
+    var mobyDickEpubPath: String!
+    
+    override func setUp() {
+        let bundle = Bundle(for: self.classForCoder)
+        
+        guard let path = bundle.path(forResource: "moby-dick", ofType: "epub") else {
+            preconditionFailure("moby-dick.epub cannot be located (or you're running tests from Xcode, => use `swift test` instead)")
+        }
+        
+        mobyDickEpubPath = path
+    }
+    
     func testParserCreation() {
         XCTAssertNotNil(Parser())
     }
