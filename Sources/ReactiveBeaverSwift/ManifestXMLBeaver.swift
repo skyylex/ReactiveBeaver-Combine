4c2ffc0ed2d465b90dc07f109adf535b369ff650
Yury Lapitsky <yury.lapitsky@gmail.com>
2020-01-02 23:03:53 +0100

    ManifestXMLBeaver implemented and tested

---
+++ Sources/ReactiveBeaverSwift/ManifestXMLBeaver.swift
@@ -0,0 +1,65 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yury Lapitsky on 02/01/2020.
+//
+
+import Foundation
+
+class ManifestXMLBeaver {
+    struct Manifest {
+        struct Item {
+            let identifier: String
+            let path: String // URL?
+            let mediaType: String // Ideally, should be enum
+            
+            init(identifier: String, path: String, mediaType: String) {
+                self.identifier = identifier
+                self.path = path
+                self.mediaType = mediaType
+            }
+            
+            init?(xmlElement: SimpleXMLElement) {
+                guard let identifier = xmlElement.attributes[Keys.id],
+                      let path = xmlElement.attributes[Keys.href],
+                      let mediaType = xmlElement.attributes[Keys.mediaType] else {
+                    return nil
+                }
+                
+                self.init(identifier: identifier, path: path, mediaType: mediaType)
+            }
+        }
+        
+        let items: [Item]
+        
+        init(items: [Item]) {
+            self.items = items
+        }
+    }
+    
+    struct Keys {
+        static let id = "id"
+        static let href = "href"
+        static let mediaType = "media-type"
+        static let manifest = "manifest"
+    }
+    
+    static func gnaw(manifestXML: SimpleXMLElement) -> Manifest? {
+        guard manifestXML.title == Keys.manifest else { return nil }
+        
+        let nullableItems = manifestXML.children.map { (element) -> Manifest.Item? in
+            guard let identifier = element.attributes[Keys.id],
+                  let path = element.attributes[Keys.href],
+                  let mediaType = element.attributes[Keys.mediaType] else {
+                return nil
+            }
+            
+            return Manifest.Item(identifier: identifier, path: path, mediaType: mediaType)
+        }
+        
+        let filteredItems = nullableItems.compactMap { $0 }
+        
+        return Manifest(items: filteredItems)
+    }
+}
--- Tests/ReactiveBeaverSwiftTests/ParserTests.swift
+++ Tests/ReactiveBeaverSwiftTests/ParserTests.swift
@@ -57,10 +57,37 @@ final class ParserTests: XCTestCase {
         }
     }
     
+    func testManifestXML() {
+        let manifestXMLString = """
+        <manifest>
+        <item id="font.stix.regular" href="fonts/STIXGeneral.otf"
+          media-type="application/vnd.ms-opentype"/>
+        <item id="font.stix.italic" href="fonts/STIXGeneralItalic.otf"
+          media-type="application/vnd.ms-opentype"/>
+        <item id="font.stix.bold" href="fonts/STIXGeneralBol.otf"
+          media-type="application/vnd.ms-opentype"/>
+        <item id="font.stix.bold.italic" href="fonts/STIXGeneralBolIta.otf"
+          media-type="application/vnd.ms-opentype"/>
+        """
+        
+        guard let xmlData = manifestXMLString.data(using: .utf8) else { preconditionFailure("Failed to create xml data") }
+        
+        let parser = SimpleXMLBeaver()
+        let result = parser.gnaw(xmlData: xmlData)
+        
+        switch result {
+        case .success(let element):
+            let manifest = ManifestXMLBeaver.gnaw(manifestXML: element)
+            XCTAssertEqual(manifest?.items.count ?? 0, 4)
+        case .failure(let error):
+            XCTAssertFalse(true, "Manifest XML parsing failed \(error)")
+        }
+    }
+    
     private func sharedTemporaryFolderURL() -> URL {
         return URL(fileURLWithPath: NSTemporaryDirectory())
     }
-    
+
     private func createTempFolderURL() -> URL {
         let newFolderURL = sharedTemporaryFolderURL().appendingPathComponent(UUID().uuidString)
         do {
@@ -68,26 +95,27 @@ final class ParserTests: XCTestCase {
         } catch (let error) {
             preconditionFailure("Cannot create temp folder due to error: \(error)")
         }
-        
+
         guard FileManager.default.fileExists(atPath: newFolderURL.path) else {
             preconditionFailure("Cannot create temp folder at \(newFolderURL.path)")
         }
-        
+
         return newFolderURL
     }
-    
+
     private func mobyDickFileURL() -> URL {
         let bundle = Bundle(for: self.classForCoder)
-        
+
         guard let path = bundle.path(forResource: "moby-dick", ofType: "epub") else {
             preconditionFailure("moby-dick.epub cannot be located (or you're running tests from Xcode, => use `swift test` instead)")
         }
-        
+
         return URL(fileURLWithPath: path)
     }
     
     static var allTests = [
         ("testParsing", testParsing),
         ("testContainerXMLParsing", testContainerXMLParsing),
+        ("testManifestXML", testManifestXML),
     ]
 }
