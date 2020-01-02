db5b5f1af3a982381810af7d70a46302a072e8c7
Yury Lapitsky <yury.lapitsky@gmail.com>
2020-01-02 23:33:57 +0100

    Moved all beavers to separate folder

---
+++ Sources/ReactiveBeaverSwift/Beavers/MetadataXMLBeaver.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yury Lapitsky on 02/01/2020.
+//
+
+import Foundation
+
+class MetadataXMLBeaver {
+    struct Metadata {
+        
+    }
+    
+    static func gnaw() -> String {
+        
+    }
+}
