52924e615cb15978ebd5fe9dbac9e1fca26cc928
Yury Lapitsky <yury.lapitsky@gmail.com>
2020-02-04 11:51:55 +0100

    Remove duplicate verification of correct paths in ZipUnarchiver

--- Sources/ReactiveBeaverSwift/ZipUnarchiver.swift
+++ Sources/ReactiveBeaverSwift/ZipUnarchiver.swift
@@ -10,14 +10,6 @@ import ZIPFoundation
 
 final class ZipUnarchiver: Unarchiver {
     func unpack(zipArchiveURL: URL, targetDirectoryURL: URL) -> Bool {
-        guard PathValidator.validate(fileURL: zipArchiveURL) else {
-            return false
-        }
-        
-        guard PathValidator.validate(directoryURL: targetDirectoryURL) else {
-           return false
-        }
-        
         do {
             try FileManager.default.unzipItem(at: zipArchiveURL, to: targetDirectoryURL)
         } catch {
